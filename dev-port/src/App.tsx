82e08b787e573f05e129b9564a188a01d61ef026
Valen DeLeon <valeriedeleon4521@gmail.com>
2025-01-08 11:27:00 -0800

    Importing styles from Figma using Tailwind

--- dev-port/src/App.tsx
+++ dev-port/src/App.tsx
@@ -1,14 +1,13 @@
 function App() {
-
   return (
     <>
-      <div className="bg-blue-500 text-white p-4 rounded-lg">
+      <div className="bg-surfacePrimary text-textPrimary font-B612 p-4">
         <h1 className="text-2xl font-bold">Hello fellow Internet user! :D</h1>
         <h2 className="text-xl font-bold">Welcome to Humble Beginnings!</h2>
         <p className="text-sm">Just testing basic Tailwind out and starting work on styling! This website is officially in the development stage!</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- dev-port/tailwind.config.js
+++ dev-port/tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          surfacePrimary: "#EFF1ED",
+          iconBgPrimary: "#BCBD8B",
+          iconColorPrimary: "#6F7806",
+          textPrimary: "#282828",
+          textSecondary: "#4A4A4A",
+      },
+      spacing: {
+        base: "16px",
+        none: "0",
+        xs: "0.25rem", // 4px
+        s: "0.5rem", // 8px
+        m: "0.75rem", // 12px
+        l: "1rem", // 16pc
+        xl: "1.25rem", // 20px (base)
+        xxl: "1.5rem", // 24px
+      },
+      fontSize: {
+        headLine1: "3rem", // 48px
+        headLine2: "2.125rem", // 34px
+        headLine3: "1.75rem", // 28px
+        headLine4: "1.125rem", // 18px
+        paragraph: "0.875em", // 14px
+      },
+      fontFamily: {
+        B612: ['"B612"', "serif"],
+      },
+    },
   },
   plugins: [],
 };
