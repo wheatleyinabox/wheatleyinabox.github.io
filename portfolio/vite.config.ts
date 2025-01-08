76283f50e9753e3e2066f9618666e25b35b71aa0
Valen DeLeon <valeriedeleon4521@gmail.com>
2025-01-08 12:01:43 -0800

    Renaming src folder, updates scripts

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://wheatleyinabox.github.io/dev-portfolio/",
+  base: "https://wheatleyinabox.github.io/portfolio/",
 })
