ed5bdbc07456ac5380c42944c4368eba09c3de94
Valen DeLeon <valeriedeleon4521@gmail.com>
2025-01-08 11:51:39 -0800

    Basic layout to showcase components as they're completed

--- dev-port/src/App.tsx
+++ dev-port/src/App.tsx
@@ -1,10 +1,24 @@
 function App() {
   return (
     <>
-      <div className="bg-surfacePrimary text-textPrimary font-B612 p-4">
-        <h1 className="text-2xl font-bold">Hello fellow Internet user! :D</h1>
-        <h2 className="text-xl font-bold">Welcome to Humble Beginnings!</h2>
-        <p className="text-sm">Just testing basic Tailwind out and starting work on styling! This website is officially in the development stage!</p>
+      <div className="bg-surfacePrimary text-textPrimary font-B612 p-xl">
+
+        <h1 className="text-headLine1 font-bold">Hello fellow Internet user! :D</h1>
+        <h2 className="text-headLine2 font-bold">Welcome to Humble Beginnings!</h2>
+        <p className="text-paragraph">
+          Just testing basic Tailwind out and starting work on styling!<br/>This website is officially in the development stage!
+        </p>
+
+        <br/>
+
+        <div className="flex flex-col p-s">
+          <h3 className="text-headLine3 font-bold">Component Building...</h3>
+          <h4 className="text-headLine4 font-bold">Blocks</h4>
+
+          <h4 className="text-headLine4 font-bold">Cards</h4>
+
+        </div>
+
       </div>
     </>
   );
