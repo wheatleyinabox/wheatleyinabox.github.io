be71d0d9e01983d5ea7ffb26c87cc91e2c63ca9d
Valen DeLeon <valeriedeleon4521@gmail.com>
2025-01-17 21:33:47 -0800

    Because of said bookcase- I gotta figure out what to do here now D:

---
+++ portfolio/src/components/Cards/NULLCard.tsx
@@ -0,0 +1,13 @@
+// I've combined my visual component with the displaying of my skills
+
+// so now I'm at a loss as to what to do here WAHHHHHHHHHHH
+
+///////////
+// IDEAS //
+///////////
+/*
+
+- Puzzle/Minigame
+- Unique Quote Generator
+
+*/
